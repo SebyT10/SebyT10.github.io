1f5ab3fecd925b154bdfae80b8cf4a1485720d97
Sebastian Trask <sebytrask@gmail.com>
2026-02-06 17:40:50 -0600

    Update ripples.js

--- js/sketches/ripples.js
+++ js/sketches/ripples.js
@@ -2,21 +2,25 @@ const rippleSketch = (p) => {
     let ripples = [];
 
     p.setup = () => {
-        let cnv = p.createCanvas(600, 400);
+        let cnv = p.createCanvas(600, 300);
         cnv.parent('canvas-parent');
     };
 
     p.draw = () => {
-        p.background(250, 247, 242);
+        p.background(253, 249, 246); // Very light parchment
         
+        // Loop through ripples backwards to remove them safely
         for (let i = ripples.length - 1; i >= 0; i--) {
             ripples[i].display();
             ripples[i].update();
-            if (ripples[i].alpha <= 0) ripples.splice(i, 1);
+            if (ripples[i].alpha <= 0) {
+                ripples.splice(i, 1);
+            }
         }
     };
 
     p.mousePressed = () => {
+        // Only trigger if click is inside canvas
         if (p.mouseX > 0 && p.mouseX < p.width && p.mouseY > 0 && p.mouseY < p.height) {
             ripples.push(new Ripple(p.mouseX, p.mouseY));
         }
@@ -29,13 +33,15 @@ const rippleSketch = (p) => {
             this.r = 0;
             this.alpha = 255;
         }
+
         update() {
             this.r += 2;
-            this.alpha -= 3;
+            this.alpha -= 4; // Fade out speed
         }
+
         display() {
             p.noFill();
-            p.stroke(148, 166, 132, this.alpha); // Sage
+            p.stroke(214, 166, 146, this.alpha); // Clay color
             p.strokeWeight(2);
             p.ellipse(this.x, this.y, this.r);
         }
