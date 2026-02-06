7d68e7108617ca5aac61073ed4520bebe961cf99
Sebastian Trask <sebytrask@gmail.com>
2026-02-06 17:40:34 -0600

    Update tree.js

--- js/sketches/tree.js
+++ js/sketches/tree.js
@@ -1,29 +1,31 @@
-// The Tree Sketch
 const treeSketch = (p) => {
     p.setup = () => {
         let cnv = p.createCanvas(600, 300);
+        cnv.parent('canvas-parent'); // Attach to div
         p.noLoop();
     };
 
     p.draw = () => {
-        p.background(253, 253, 253);
-        p.stroke(163, 177, 138); // Sage
+        p.background(255); // White background
+        p.stroke(148, 166, 132); // Sage Green
         p.translate(p.width / 2, p.height);
         branch(80);
     };
 
-    p.mouseMoved = () => p.redraw();
+    p.mouseMoved = () => {
+        p.redraw();
+    };
 
     function branch(len) {
         p.line(0, 0, 0, -len);
         p.translate(0, -len);
         if (len > 4) {
             p.push();
-            p.rotate(p.mouseX / 200);
+            p.rotate(p.mouseX / 200); // Interaction
             branch(len * 0.67);
             p.pop();
             p.push();
-            p.rotate(-p.mouseX / 200);
+            p.rotate(-p.mouseX / 200); // Interaction
             branch(len * 0.67);
             p.pop();
         }
